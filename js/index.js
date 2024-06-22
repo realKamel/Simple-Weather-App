21e41b111e437e0f98591bfcf722348d8b47b1d0
Abdelrahman <abdo.aly.k.m@gmail.com>
2024-06-22 17:26:45 +0300

    fin of js

--- js/index.js
+++ js/index.js
@@ -24,23 +24,22 @@ const Days = [
 ];
 const directions = {
 	N: "North wind",
-	NNE: "North northeast wind",
+	NNE: "North wind",
 	NE: "North East wind ",
-	ENE: "East northeast wind",
+	ENE: "East wind",
 	E: "East wind",
-	ESE: "East southeast wind",
+	ESE: "East wind",
 	SE: "South East wind",
-	SSE: "South southeast wind ",
+	SSE: "South wind ",
 	S: "South wind",
-	SSW: "South southwest wind ",
+	SSW: "South wind ",
 	SW: "South West wind",
-	WSW: "West southwest wind",
+	WSW: "West wind",
 	W: "West wind",
-	WNW: "West northwest wind",
+	WNW: "West wind",
 	NW: "North West wind",
-	NNW: "North northwest wind",
+	NNW: "North wind",
 };
-
 const skinsList = [
 	"clear-sky",
 	"cloudy-sky",
@@ -51,7 +50,10 @@ const skinsList = [
 	"blizzard-sky",
 	"drizzle-sky",
 	"rain-sky",
+	"sunny-sky"
 ];
+// to handle user location
+let userLocation = sessionStorage.getItem( "userLocation" );
 // today data
 const city = document.getElementById( "my-city" );
 const myDate = document.querySelector( ".date .content p" );
@@ -77,60 +79,47 @@ const nextLDegree = document.getElementById( "next-Ldegree" );
 const nextKindIcon = document.querySelector( ".next-kind img" );
 const nextKindName = document.querySelector( ".next-kind span.name" );
 
-//
+// search method
 const searchInput = document.getElementById( "floatingSearch" );
 const searchBtn = document.getElementById( "searchBtn" );
 
-//
 const dangerAlert = document.getElementById( "dangerAlert" );
 
 // data list for search results
-
 const dataList = document.getElementById( "datalistOptions" );
 
 async function getWeatherData ( q ) {
 	try {
+		dangerAlert.classList.add( "d-none" );
 		let response = await fetch(
 			`http://api.weatherapi.com/v1/forecast.json?key=2507a24fdbbf42b28b4130022241306&q=${ q }&days=3`
 		);
-		let data = await response.json();
+		if ( !response.ok ) {
+			dangerAlert.classList.remove( "d-none" );
+			throw new Error( `HTTP error! status: ${ response.status }` );
+		}
 		if ( response.status !== 200 ) {
-			if ( data.error.code === 1006 ) console.log( "No matching location found." );
+			if ( data.error.code === 1006 )
+				console.log( "No matching location found." );
 			console.log( dangerAlert );
 			dangerAlert.classList.remove( "d-none" );
 		}
-		return data;
+		return await response.json();
 	} catch ( error ) {
-		console.log( error );
-	} finally {
-		dangerAlert.classList.add( "d-none" );
+		console.error( "Error fetching weather data:", error );
+		return null;
 	}
 }
 
-async function tomorrow ( obj ) {
-	let tomorrowDate = new Date( obj.date );
-	let tomorrowName = Days[ tomorrowDate.getDay() ]; // getDay() return day number as Sunday - Saturday : 0 - 6
-
-	tomorrowDayName.innerHTML = tomorrowName;
-
-	tomorrowHDegree.innerHTML = `${ obj.day.maxtemp_c }°C`;
-	tomorrowLDegree.innerHTML = `${ obj.day.mintemp_c }°C`;
-
-	tomorrowKindIcon.src = `https:${ obj.day.condition.icon }`;
-	tomorrowKindName.innerHTML = obj.day.condition.text;
+async function updateForecast ( dayElement, obj ) {
+	let objDate = new Date( obj.date );
+	dayElement.dayName.innerHTML = Days[ objDate.getDay() ]; // getDay() return day number as Sunday - Saturday : 0 - 6
+	dayElement.hDegree.innerHTML = `${ obj.day.maxtemp_c }°C`;
+	dayElement.lDegree.innerHTML = `${ obj.day.mintemp_c }°C`;
+	dayElement.kindIcon.src = `https:${ obj.day.condition.icon }`;
+	dayElement.kindName.innerHTML = obj.day.condition.text;
 }
-async function nextDay ( obj ) {
-	let nextDayDate = new Date( obj.date );
-	let nextDayObjName = Days[ nextDayDate.getDay() ]; // getDay() return day number as Sunday - Saturday : 0 - 6
-	nextDayName.innerHTML = nextDayObjName;
 
-	nextHDegree.innerHTML = `${ obj.day.maxtemp_c }°C`;
-	nextLDegree.innerHTML = `${ obj.day.mintemp_c }°C`;
-
-	nextKindIcon.src = `https:${ obj.day.condition.icon }`;
-
-	nextKindName.innerHTML = obj.day.condition.text;
-}
 async function weather ( q ) {
 	try {
 		loader.classList.remove( "d-none" );
@@ -142,23 +131,32 @@ async function weather ( q ) {
 		const date = new Date( `${ data.location.localtime }` );
 
 		city.innerHTML = `${ data.location.name } ,${ data.location.country }`;
-
 		myDate.innerHTML = `Today , ${ date.getDate() }  ${ months[ date.getMonth() ] }`;
-
 		myDegree.innerHTML = `${ data.current.temp_c }°`;
 		myCond.innerHTML = `${ data.current.condition.text }`;
-
-		await changeSkin( data.current.condition.text.toLowerCase() );
 		myIcon.src = `https:${ data.current.condition.icon }`;
 		windSpeed.innerHTML = `${ data.current.wind_kph }  `;
 		rainChance.innerHTML = `${ data.current.humidity } `;
 		windDir.innerHTML = `${ directions[ data.current.wind_dir ] } `;
 		windArrow.style.transform = `rotate(${ data.current.wind_degree - 45 }deg)`;
-
 		console.log( "writing data to html" );
 
-		await tomorrow( tomorrowObj );
-		await nextDay( nextDayObj );
+		await changeSkin( data.current.condition.text.toLowerCase() );
+		await updateForecast( {
+			dayName: tomorrowDayName,
+			hDegree: tomorrowHDegree,
+			lDegree: tomorrowLDegree,
+			kindIcon: tomorrowKindIcon,
+			kindName: tomorrowKindName
+		}, tomorrowObj );
+		await updateForecast( {
+			dayName: nextDayName,
+			hDegree: nextHDegree,
+			lDegree: nextLDegree,
+			kindIcon: nextKindIcon,
+			kindName: nextKindName
+		}, nextDayObj );
+
 		console.log( "done all working" );
 	} catch ( error ) {
 		console.log( "Error Happened in weather()" );
@@ -168,71 +166,50 @@ async function weather ( q ) {
 }
 
 async function changeSkin ( weatherState ) {
-	console.log( myContainer );
 	for ( const skin of skinsList ) {
 		if ( myContainer.classList.contains( skin ) )
 			myContainer.classList.remove( skin );
 	}
 	console.log( "Weather Change to", weatherState );
 
+
 	for ( const iterator of skinsList ) {
-		if ( iterator.search( weatherState ) ) {
+		let res = iterator.split( "-" )
+		if ( weatherState.includes( res[ 0 ] ) ) {
+			console.log( weatherState, res[ 0 ] );
 			myContainer.classList.add( iterator );
+			break;
 		}
-	}
-	/* if ( weatherState === "sunny" ) {
-		myContainer.classList.add( "sunny-sky" );
-	}
-	else if ( weatherState === "clear" ) {
-		myContainer.classList.add( "clear-sky" );
-	}
-	else if ( weatherState.includes( "rain" ) ) {
-		myContainer.classList.add( "rain-sky" );
-	}
-	else if ( weatherState.includes( "drizzle" ) ) {
-		myContainer.classList.add( "drizzle-sky" );
-	}
-	else if ( weatherState.includes( "blizzard" ) ) {
-		myContainer.classList.add( "blizzard-sky" );
-	}
-	else if ( weatherState.includes( "mist" ) ) {
-		myContainer.classList.add( "mist-sky" );
-	}
-	else if ( weatherState.includes( "overcast" ) ) {
-		myContainer.classList.add( "overcast-sky" );
-	}
-	else if ( weatherState.includes( "thunder" ) ) {
-		myContainer.classList.add( "thunder-sky " );
-	}
-	else if ( weatherState.includes( "sleet" ) ) {
-		myContainer.classList.add( "sleet-sky" );
-	}
-	else if ( weatherState.includes( "cloudy" ) ) {
-		myContainer.classList.add( "cloudy-sky" );
-	} */
+	};
 }
 
-// for GetApi and locating user
-function successCallback ( position ) {
-	const latitude = position.coords.latitude;
-	const longitude = position.coords.longitude;
-	console.log( `Latitude: ${ latitude }, Longitude: ${ longitude }` );
-	weather( `${ latitude },${ longitude }` );
+// for GeoApi and locating user
+if ( userLocation === null ) {
+	function successCallback ( position ) {
+		const latitude = position.coords.latitude;
+		const longitude = position.coords.longitude;
+		console.log( `Latitude: ${ latitude }, Longitude: ${ longitude }` );
+		sessionStorage.setItem( "userLocation", JSON.stringify( `${ latitude },${ longitude }` ) );
+		weather( `${ latitude },${ longitude }` );
+	}
+	function errorCallback ( error ) {
+		console.warn( `ERROR(${ error.code }): ${ error.message }` );
+	}
+	const options = {
+		enableHighAccuracy: true, // Use high accuracy if available
+		timeout: 5000, // Timeout after 5 seconds
+		maximumAge: 0, // Do not use cached position
+	};
+	navigator.geolocation.getCurrentPosition(
+		successCallback,
+		errorCallback,
+		options
+	);
+} else {
+	let loc = JSON.parse( userLocation );
+	weather( loc );
 }
-function errorCallback ( error ) {
-	console.warn( `ERROR(${ error.code }): ${ error.message }` );
-}
-const options = {
-	enableHighAccuracy: true, // Use high accuracy if available
-	timeout: 5000, // Timeout after 5 seconds
-	maximumAge: 0, // Do not use cached position
-};
 
-navigator.geolocation.getCurrentPosition(
-	successCallback,
-	errorCallback,
-	options
-);
 
 /* To Handel Search and Suggestions for locations */
 searchBtn.addEventListener( "click", function () {
